b19cdfb1dc28d9b504dbf17623fe1cdc23799b3f
kfil <koafil@gmail.com>
2022-06-14 13:20:03 +0300

    Добавили nodemon

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = 5000;
 const app = express();
 
 app.get('/',(req,res)=> {
-  res.status(200).json("Сервер работает");
+  res.status(200).json("Сервер работает!");
 });
 
 app.listen(PORT, ()=>console.log(`Server started on port ${PORT}`));
